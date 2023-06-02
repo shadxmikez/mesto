b2e6f12a5e5bdb414a0cde17372124065e1e493a
Dave Ivashevic <shadxmikez@yndex.ru>
2023-06-02 17:42:26 +0300

    Работа над ошибками

--- index.js
+++ index.js
@@ -2,40 +2,41 @@
 let popup = document.querySelector('.popup');
 let popunOpened = document.querySelector('.profile__edit-button');
 let popunClose = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_description');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 let formElement = document.querySelector('.popup__form');
 let nameTitle = document.querySelector('.profile__title');
 let jobSub = document.querySelector('.profile__subtitle');
 let popupSubmit = document.querySelector('.popup__submit');
 
-	popunOpened.addEventListener('click', () => {
-		popup.classList.add('popup__opened');
-	});
+popunOpened.addEventListener('click', () => {
+	popup.classList.add('popup_opened');
+});
 
-	popunClose.addEventListener('click', () => {
-		popup.classList.remove('popup__opened');
-	});
+popunClose.addEventListener('click', () => {
+	popup.classList.remove('popup_opened');
+});
 
-	document.querySelector('input[name="username"]').defaultValue = 'Жак-Ив Кусто'
-	document.querySelector('input[name="description"]').defaultValue = 'Исследователь океана'
+document.querySelector('input[name="username"]').defaultValue = 'Жак-Ив Кусто'
+document.querySelector('input[name="description"]').defaultValue = 'Исследователь океана'
 
-	function handleFormSubmit (evt) {
-    evt.preventDefault();
-		nameTitle.textContent = nameInput.value;
-		jobSub.textContent = jobInput.value;
-	}
-	formElement.addEventListener('submit', handleFormSubmit);
-	
-	popupSubmit.addEventListener('click', () => {
-		popup.classList.remove('popup__opened');
-	});
+function handleFormSubmit(evt) {
+	evt.preventDefault();
+	nameInput.value = nameTitle.textContent;
+	jobInput.value = jobSub.textContent; 
+	popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
 
 
 
+ /* nameTitle.textContent = nameInput.value; 
+  jobSub.textContent = jobInput.value; */
 
-	
 
-	
+
+
+
 
 
